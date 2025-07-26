bf7b38b437dccba2510f5b954e4fd0ddd0f99b32
shruti-2517 <shruti170205@gmail.com>
2025-07-26 19:06:30 +0530

    Corrected an error

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,27 +2,27 @@ const User = require("../models/authModels");
 
 exports.getProfileInfoController = async (req, res) => {
     const userId = req.user._id;
-    const user = await User.findOne({ userId }, { email: 1, name: 1, public: 1});
+    const user = await User.findOne({ userId }, { email: 1, name: 1, public: 1 });
     if (!user) {
         return res.status(401).json({ error: "Cannot retrieve profile information" });
     }
-     return res.status(200).json({ user });
+    return res.status(200).json({ user });
 };
 
 exports.profileVisibilityController = async (req, res) => {
     const userId = req.user._id;
-    const public = req.public;
-    await User.findOneAndUpdate({ userId }, { $set: { public: public } });
-    if (!user) {
-        return res.status(401).json({ error: "Cannot change profile visibility" });
+    const { public } = req.body;
+    const updatedUser = await User.findOneAndUpdate({ userId }, { $set: { public: public } }, { new: true });
+    if (!updatedUser) {
+        return res.status(404).json({ error: "User not found or update failed" });
     }
-    return res.status(200).json({message: "Visibilty updated"});
+    return res.status(200).json({ message: "Visibilty updated" });
 };
 
 exports.getPublicProfilesController = async (req, res) => {
     const users = await User.find({ public: true });
-    if (!user) {
-        return res.status(401).json({ error: "Cannot fetch public profiles" });
+    if (!users.length === 0) {
+        return res.status(401).json({ error: "Np public profiles found" });
     }
     return res.status(200).json({ users });
 };
